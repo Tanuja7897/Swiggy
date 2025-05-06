f753761a6d35be14b40b92f09ab3ce09691cc57f
Tanuja <tanuja7897@gmail.com>
2025-05-06 23:47:43 +0530

    change

--- frontend/src/component/Body.jsx
+++ frontend/src/component/Body.jsx
@@ -4,7 +4,18 @@ import ResWithOnlineDeli from "./ResWithOnlineDeli";
 import { UserProvider } from "../utils/context";
 import BestPlacesCities from "./BestPlacesCities";
 function Body() {
-
+  return (
+    <UserProvider>
+      <div className='w-full mt-4'>
+        <div className='w-[80%] flex flex-col ml-[10%] overflow-hidden'>
+          <Quizine />
+          <TopRest />
+          <ResWithOnlineDeli/>
+          <BestPlacesCities/>
+        </div>
+      </div>
+    </UserProvider>
+  );
 }
 
 export default Body;
