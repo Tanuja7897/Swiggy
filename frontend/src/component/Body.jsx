5ce9c814a6afbfa5e3a01df403bfff618b5660cb
Tanuja <tanuja7897@gmail.com>
2025-05-06 23:45:15 +0530

    change

--- frontend/src/component/Body.jsx
+++ frontend/src/component/Body.jsx
@@ -6,14 +6,7 @@ import BestPlacesCities from "./BestPlacesCities";
 function Body() {
   return (
     <UserProvider>
-      <div className='w-full mt-4'>
-        <div className='w-[80%] flex flex-col ml-[10%] overflow-hidden'>
-          <Quizine />
-          <TopRest />
-          <ResWithOnlineDeli/>
-          <BestPlacesCities/>
-        </div>
-      </div>
+
     </UserProvider>
   );
 }
